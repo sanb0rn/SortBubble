0d3da5c35d2f2c6d38aac768783e35dc98a081cc
sanb0rn <43263145+sanb0rn@users.noreply.github.com>
2020-05-21 00:54:57 +0300

    Add files via upload

--- sort.js
+++ sort.js
@@ -1,12 +1,12 @@
 const blockSort = document.querySelector(".blockSort");
 
 function generateNumbers() {
-  
-  for (let i = 0; i < 10; i ++) {
+  for (let i = 0; i < 10; i++) {
     const value = Math.floor(Math.random() * 100);
 
     const blockNumber = document.createElement("div");
     blockNumber.classList.add("element");
+    blockNumber.style.transform = `translateX(${(i-4.5) * 75}px)`;
 
     blockNumber.innerHTML = value;
     blockSort.appendChild(blockNumber);
@@ -14,11 +14,17 @@ function generateNumbers() {
 }
 
 function swap(element1, element2) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const style1 = window.getComputedStyle(element1);
     const style2 = window.getComputedStyle(element2);
 
-    window.requestAnimationFrame(function() {
+    const transform1 = style1.getPropertyValue("transform");
+    const transform2 = style2.getPropertyValue("transform");
+
+    element1.style.transform = transform2;
+    element2.style.transform = transform1;
+
+    window.requestAnimationFrame(function () {
       setTimeout(() => {
         blockSort.insertBefore(element2, element1);
         resolve();
@@ -27,15 +33,14 @@ function swap(element1, element2) {
   });
 }
 
-btnSort.onclick=async function bubbleSort() {
-
+btnSort.onclick = async function bubbleSort() {
   let elements = document.querySelectorAll(".element");
   for (let i = 0; i < elements.length - 1; i += 1) {
     for (let j = 0; j < elements.length - i - 1; j += 1) {
       elements[j].style.backgroundColor = "red";
       elements[j + 1].style.backgroundColor = "red";
 
-      await new Promise(resolve =>
+      await new Promise((resolve) =>
         setTimeout(() => {
           resolve();
         }, 1000)
@@ -56,6 +61,6 @@ btnSort.onclick=async function bubbleSort() {
     elements[elements.length - i - 1].style.backgroundColor = "green";
   }
   elements[0].style.backgroundColor = "green";
-}
+};
 
 generateNumbers();
